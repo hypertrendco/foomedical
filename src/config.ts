86226873b55cd642aac09e142ccaf78ea7e19eee
hypertrendco <32074330+hypertrendco@users.noreply.github.com>
2026-03-31 11:53:14 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,4 @@
 export const MEDPLUM_PROJECT_ID = '9602358d-eeb0-4de8-bccf-e2438b5c9162';
 export const MEDPLUM_GOOGLE_CLIENT_ID = '679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com';
 export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LcP4J4sAAAAABUbHWhe-y-kqHvcP7Yrlj40MQ9y';
+export const MEDPLUM_RECAPTCHA_SECRET_KEY = '6LcP4J4sAAAAAKxRYRcrcWsjt0kPNAIcnoAsKyIR';
